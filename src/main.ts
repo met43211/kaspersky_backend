c58118c108106a1258eef75332c57c9805d06244
met43211 <matvey-anashkin@yandex.ru>
2024-11-06 12:45:56 +0300

    feat: enable all requests cors

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AuthGuard } from './guards/auth-guard';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.useGlobalGuards(new AuthGuard());
   await app.listen(4000);
 }
