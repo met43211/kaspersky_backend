caf7106a83dffdb37153205dfe53c2b7e95bba7e
met43211 <matvey-anashkin@yandex.ru>
2024-11-13 13:16:06 +0300

    feat: user validation

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AuthGuard } from './guards/auth-guard';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   app.enableCors();
   app.useGlobalGuards(new AuthGuard());
   await app.listen(4000);
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,9 +1,14 @@
 import { EPrizesTypes } from 'src/public-types/prizes-types.type';
-
+import { IsEmail, IsEnum, IsString, Matches } from 'class-validator';
 export class CreateUserDto {
+  @IsString()
   name: string;
+  @IsEmail()
   email: string;
+  @Matches(/^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/)
   phone: string;
+  @IsString()
   job: string;
+  @IsEnum(EPrizesTypes)
   level: EPrizesTypes;
 }
